0a419fe4acb19152a18fc62560ba5bc6352da02c
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-04-17 16:18:50 -0400

    On my way there. Tests done, working on code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,36 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Main {
+    //Group by name
+    // Group by price
+    // Count how many times seen
+    // Count errors
+    //Format
+    //More detailed steps
+    //Created Expression
+    // process through streams
+    //Apply lambdas to process output
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+
+
+        //Pattern
+        String patternString = "##";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(result);
+        // Stream
+        String[] input = result.split("\\r?\\n");
+        Stream<String> stream = Arrays.stream(input);
+        //applying pattern to Stream
+
+        //Lambdas to process output
         return result;
     }
 
---
+++ src/test/java/test.java
@@ -0,0 +1,56 @@
+import jdk.internal.module.ModuleHashesBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class test {
+    public test() {
+    }
+    @Before
+    public void Init(){
+        String result;
+    }
+
+
+    @Test
+    public void doesNotContainHashTagTest(){
+        String inputString = result;
+        boolean testcode = !inputString.contains("##");
+        Assert.assertTrue(testcode);
+    }
+    @Test
+    public void testStringContainsErrorFourTimes() {
+        String inputString = result;
+        int count = 0;
+        int fromIndex = 0;
+        while ((fromIndex = inputString.indexOf("Error", fromIndex)) != -1) {
+            count++;
+            fromIndex++;
+        }
+        Assert.assertEquals(4, count);
+    }
+    @Test
+    public void doesNotContainExpirationTest(){
+        String inputString = result;
+        boolean testcode = !inputString.contains("expiration");
+        Assert.assertTrue(testcode);
+    }
+    @Test
+    public void testStringContainsCookiesSpelledCorrectly() {
+        String inputString = result;
+        boolean result = inputString.contains("Cookies")
+                && !inputString.contains("CoOkieO")
+                && !inputString.contains("COokIeS")
+                && !inputString.contains("COOkIeS")
+                && !inputString.contains("Co0kieS")
+                && !inputString.contains("COOkieS");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void testStringContainsNameFourTimes() {
+        String inputString = result;
+        int count = inputString.split("name", -1).length - 1;
+        Assert.assertEquals(4, count);
+    }
+}
+
